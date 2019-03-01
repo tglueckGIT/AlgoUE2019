043af795cd3cac362978565281faa1dd970ea01c
Tobias Glueck <glueck_t@gmx.at>
2019-03-01 19:48:26 +0100

    erstellt "Death should not have taken thee.txt" und "test.cpp"

---
+++ A0/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+cout << "King!\nWhat now?\nHelp!\nOh what do you want?\nThey!\nRight..\nThey took the princess\nand won't give her back!\nHero should...\nNot exist.\nHmm, got it!\nEh?\nNo need to fear, you be the hero!\nSet up, let down!\nJimmying the lock and a brand new stock\nOf any old item to get you\ngoing!\nParty on up and to get you pumped and fight\nOh tell me you're just kidding, right?\nFine so ever if you're in trouble talk to the peasants, okay?\nAnd with a smash go beat that boss down!\n;
+
+return 0;
+}
+
